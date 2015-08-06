f5fc5625fcef5908e3beabc54911fcafc24bdfac
Dane Shih <daneshih1125@gmail.com>
2015-08-06 21:56:26 +0800

    stupid list

---
+++ slist.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list_item {
+	struct list_item *next;
+} LIST_ITEM;
+
+#define INIT_LIST(LIST_NAME) \
+ 	LIST_ITEM  *LIST_NAME##_HEAD = NULL;
+
+#define LIST_ENTRY(ptr, type, member) \
+	((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
+
+struct foo {
+	int val;
+	LIST_ITEM list;
+};
+
+void add_list_item(LIST_ITEM *item, LIST_ITEM **head)
+{
+	LIST_ITEM *p;
+
+
+	if (*head == NULL) {
+		*head = item;
+	} else {
+		p = *head;
+		while (p->next != NULL)
+			p = p->next;
+		p->next = item;
+	}
+}
+
+void visit_list_item(LIST_ITEM *head) {
+	LIST_ITEM *p = head;
+	struct foo *foo;
+	int i = 0;
+
+	while (p) {
+		foo = LIST_ENTRY(p, struct foo, list);
+		printf("num %d val %d\n", ++i, foo->val);
+		p = p->next;
+	}
+}
+
+
+void reverse_list_item(LIST_ITEM **head) {
+	LIST_ITEM *p, *next;
+	
+	p = *head;
+	while (p->next != NULL) {
+		next = p->next;
+		p->next = next->next;
+		next->next = *head;
+		*head = next;
+	}
+}
+
+
+INIT_LIST(FOO);
+
+int main()
+{
+	int i, *val;	
+	struct foo *pfoo;
+
+	for (i = 0; i < 10; i++) {
+		pfoo = (struct foo *) malloc(sizeof(struct foo));
+		pfoo->val = i + 100;
+		add_list_item(&pfoo->list, &FOO_HEAD);
+	}
+	
+	visit_list_item(FOO_HEAD);
+	reverse_list_item(&FOO_HEAD);
+	visit_list_item(FOO_HEAD);
+
+}
